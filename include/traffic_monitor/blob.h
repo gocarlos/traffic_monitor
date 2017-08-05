d1818f3f11f149e03e2f4920a5ace770751d0798
Carlos Gomes <kmartinho8@gmail.com>
2017-08-06 00:54:46 +0200

    style [ci skip]

--- include/traffic_monitor/blob.h
+++ include/traffic_monitor/blob.h
@@ -1,42 +1,40 @@
 // (c) 2017 Vigilatore
 
-
 #ifndef MY_BLOB
 #define MY_BLOB
 
-#include<opencv2/core/core.hpp>
-#include<opencv2/highgui/highgui.hpp>
-#include<opencv2/imgproc/imgproc.hpp>
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
 
 #include <glog/logging.h>
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////
 class Blob {
-public:
-    // member variables ///////////////////////////////////////////////////////////////////////////
-    std::vector<cv::Point> currentContour;
+ public:
+  // member variables
+  // ///////////////////////////////////////////////////////////////////////////
+  std::vector<cv::Point> currentContour;
 
-    cv::Rect currentBoundingRect;
+  cv::Rect currentBoundingRect;
 
-    std::vector<cv::Point> centerPositions;
+  std::vector<cv::Point> centerPositions;
 
-    double dblCurrentDiagonalSize;
-    double dblCurrentAspectRatio;
+  double dblCurrentDiagonalSize;
+  double dblCurrentAspectRatio;
 
-    bool blnCurrentMatchFoundOrNewBlob;
+  bool blnCurrentMatchFoundOrNewBlob;
 
-    bool blnStillBeingTracked;
+  bool blnStillBeingTracked;
 
-    int intNumOfConsecutiveFramesWithoutAMatch;
+  int intNumOfConsecutiveFramesWithoutAMatch;
 
-    cv::Point predictedNextPosition;
+  cv::Point predictedNextPosition;
 
-    // function prototypes ////////////////////////////////////////////////////////////////////////
-    Blob(std::vector<cv::Point> _contour);
-    void predictNextPosition(void);
-
+  // function prototypes
+  // ////////////////////////////////////////////////////////////////////////
+  Blob(std::vector<cv::Point> _contour);
+  void predictNextPosition(void);
 };
 
-#endif    // MY_BLOB
-
-
+#endif  // MY_BLOB
