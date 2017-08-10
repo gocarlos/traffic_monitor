d4a6511b09c1ebafa04c81bd1010f68b82a068e8
Carlos Gomes <kmartinho8@gmail.com>
2017-08-10 10:47:41 +0200

    fixes

--- src/main.cc
+++ src/main.cc
@@ -42,18 +42,18 @@ int RunServer() {
                        shared_ptr<WsServer::Message> message) {
     auto message_str = message->string();
 
-    cout << "Server: Message received: \"" << message_str << "\" from "
-         << connection.get() << endl;
+    std::cout << "Server: Message received: \"" << message_str << "\" from "
+              << connection.get() << endl;
 
-    cout << "Server: Sending message \"" << message_str << "\" to "
-         << connection.get() << endl;
+    std::cout << "Server: Sending message \"" << message_str << "\" to "
+              << connection.get() << endl;
 
     auto send_stream = make_shared<WsServer::SendStream>();
     *send_stream << message_str;
     // connection->send is an asynchronous function
     connection->send(send_stream, [](const SimpleWeb::error_code &ec) {
       if (ec) {
-        cout << "Server: Error sending message. " <<
+        std::cout << "Server: Error sending message. " <<
             // See
             // http://www.boost.org/doc/libs/1_55_0/doc/html/boost_asio/reference.html,
             // Error Codes for error code meanings
@@ -73,8 +73,8 @@ int RunServer() {
   // See RFC 6455 7.4.1. for status codes
   echo.on_close = [](shared_ptr<WsServer::Connection> connection, int status,
                      const string & /*reason*/) {
-    cout << "Server: Closed connection " << connection.get()
-         << " with status code " << status << endl;
+    std::cout << "Server: Closed connection " << connection.get()
+              << " with status code " << status << std::endl;
   };
 
   // See
@@ -97,89 +97,88 @@ int RunServer() {
       auto web_root_path = boost::filesystem::canonical("webserver");
       auto path = boost::filesystem::canonical(web_root_path / request->path);
       // Check if path is within web_root_path
-      if ((distance(web_root_path.begin(), web_root_path.end()) >
-               distance(path.begin(), path.end()) ||
-           not equal(web_root_path.begin(), web_root_path.end(),
+      if (distance(web_root_path.begin(), web_root_path.end()) >
+              distance(path.begin(), path.end()) ||
+          (not equal(web_root_path.begin(), web_root_path.end(),
                      path.begin()))) {
         throw invalid_argument("path must be within root path");
       }
       if (boost::filesystem::is_directory(path)) {
+        path /= "index.html";
       }
-      path /= "index.html";
-    }
 
-    SimpleWeb::CaseInsensitiveMultimap header;
+      SimpleWeb::CaseInsensitiveMultimap header;
 
-    //    Uncomment the following line to enable Cache-Control
-    //    header.emplace("Cache-Control", "max-age=86400");
+      //    Uncomment the following line to enable Cache-Control
+      //    header.emplace("Cache-Control", "max-age=86400");
 
-    auto ifs = make_shared<ifstream>();
-    ifs->open(path.string(), ifstream::in | ios::binary | ios::ate);
+      auto ifs = make_shared<ifstream>();
+      ifs->open(path.string(), ifstream::in | ios::binary | ios::ate);
 
-    if (*ifs) {
-      auto length = ifs->tellg();
-      ifs->seekg(0, ios::beg);
+      if (*ifs) {
+        auto length = ifs->tellg();
+        ifs->seekg(0, ios::beg);
 
-      header.emplace("Content-Length", to_string(length));
-      response->write(header);
+        header.emplace("Content-Length", to_string(length));
+        response->write(header);
 
-      // Trick to define a recursive function within this scope (for example
-      // purposes)
-      class FileServer {
-       public:
-        static void read_and_send(
-            const shared_ptr<HttpServer::Response> &response,
-            const shared_ptr<ifstream> &ifs) {
-          // Read and send 128 KB at a time
-          static vector<char> buffer(
-              131072);  // Safe when server is running on one thread
-          streamsize read_length;
-          if ((read_length = ifs->read(&buffer[0], buffer.size()).gcount()) >
-              0) {
-            response->write(&buffer[0], read_length);
-            if (read_length == static_cast<streamsize>(buffer.size())) {
-              response->send([response, ifs](const SimpleWeb::error_code &ec) {
-                if (!ec) {
-                  read_and_send(response, ifs);
-                } else {
-                  cerr << "Connection interrupted" << endl;
-                }
-              });
+        // Trick to define a recursive function within this scope (for example
+        // purposes)
+        class FileServer {
+         public:
+          static void read_and_send(
+              const shared_ptr<HttpServer::Response> &response,
+              const shared_ptr<ifstream> &ifs) {
+            // Read and send 128 KB at a time
+            static vector<char> buffer(
+                131072);  // Safe when server is running on one thread
+            streamsize read_length;
+            if ((read_length = ifs->read(&buffer[0], buffer.size()).gcount()) >
+                0) {
+              response->write(&buffer[0], read_length);
+              if (read_length == static_cast<streamsize>(buffer.size())) {
+                response->send(
+                    [response, ifs](const SimpleWeb::error_code &ec) {
+                      if (not ec)
+                        read_and_send(response, ifs);
+                      else
+                        std::cerr << "Connection interrupted" << endl;
+                    });
+              }
             }
           }
-        }
-      };
-      FileServer::read_and_send(response, ifs);
-    } else
-      throw invalid_argument("could not read file");
-  } catch (const exception &e) {
-    response->write(SimpleWeb::StatusCode::client_error_bad_request,
-                    "Could not open path " + request->path + ": " + e.what());
-  }
-};
+        };
+        FileServer::read_and_send(response, ifs);
+      } else
+        throw invalid_argument("could not read file");
+    } catch (const exception &e) {
+      response->write(SimpleWeb::StatusCode::client_error_bad_request,
+                      "Could not open path " + request->path + ": " + e.what());
+    }
+  };
 
-http_server.on_error = [](shared_ptr<HttpServer::Request> /*request*/,
-                          const SimpleWeb::error_code & /*ec*/) {
-  // Handle errors here
-};
+  http_server.on_error = [](shared_ptr<HttpServer::Request> /*request*/,
+                            const SimpleWeb::error_code & /*ec*/) {
+    // Handle errors here
+  };
 
-thread http_server_thread([&http_server]() {
-  // Start server
-  LOG(INFO) << "Webserver started";
-  http_server.start();
-});
+  thread http_server_thread([&http_server]() {
+    // Start server
+    LOG(INFO) << "Webserver started";
+    http_server.start();
+  });
 
-thread ws_server_thread([&ws_server]() {
-  // Start WS-server
-  LOG(INFO) << "Websocketsserver started";
-  ws_server.start();
-});
-// Wait for server to start so that the client can connect
-this_thread::sleep_for(chrono::seconds(1));
+  thread ws_server_thread([&ws_server]() {
+    // Start WS-server
+    LOG(INFO) << "Websocketsserver started";
+    ws_server.start();
+  });
+  // Wait for server to start so that the client can connect
+  this_thread::sleep_for(chrono::seconds(1));
 
-http_server_thread.join();
-ws_server_thread.join();
-return 0;
+  http_server_thread.join();
+  ws_server_thread.join();
+  return 0;
 }
 
 int main(int argc, char *argv[]) {
@@ -188,11 +187,9 @@ int main(int argc, char *argv[]) {
   cxxopts::Options options("Traffic Monitor", "todo: longer app description");
 
   // clang-format off
-  options.add_options()
-		  ("d,debug", "Enable debugging")
-		  ("g,withgui", "Enable gui")
-		  ("l,logfile", "Logging file name", cxxopts::value<std::string>()
-		  );
+	options.add_options()("d,debug", "Enable debugging")("g,withgui",
+			"Enable gui")("l,logfile", "Logging file name",
+			cxxopts::value<std::string>());
   // clang-format on
 
   options.parse(argc, argv);
@@ -209,12 +206,12 @@ int main(int argc, char *argv[]) {
       << "Starting traffic monitor in debug mode";
   LOG(INFO) << "Path to logging file is: " << Settings::path_to_log_file_;
 
+  RunServer();
   //  Tracker tracker;
 
   //  tracker.input_ = Tracker::camera;
   //  tracker.camera_number_ = 0;
   //  tracker.run();
-  RunServer();
 
   return 0;
 }
