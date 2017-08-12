5fa9750add66484f9f0edc075210dc023ccfa0c2
Carlos Gomes <gocarlos@users.noreply.github.com>
2017-08-12 09:49:44 +0200

    fixes

--- cmake/Settings.cmake
+++ cmake/Settings.cmake
@@ -3,7 +3,7 @@
 IF(CREATE_SNAP_APP)
   SET(INSTALL_DIRECTORE_SETTINGS "/snap/traffic-monitor/current/traffic-monitor/")
   MESSAGE("Going to move the settings files to ${INSTALL_DIRECTORE_SETTINGS}")
-  FILE(WRITE ${CMAKE_CURRENT_LIST_DIR}/../include/traffic-monitor/paths.h
+  FILE(WRITE ${CMAKE_CURRENT_LIST_DIR}/../include/traffic_monitor/paths.h
     "// Auto-generated headers file, do not edit, will be overwritten by cmake.
     // (c) 2017 Vigilatore \n
     #ifndef INCLUDE_TRAFFIC_MONITOR_CONFIG_H_
@@ -22,7 +22,7 @@ ELSE()
   SET(INSTALL_DIRECTORE_SETTINGS "${CMAKE_CURRENT_LIST_DIR}/../")
   MESSAGE("Going to move the settings files to ${INSTALL_DIRECTORE_SETTINGS}")
 
-  FILE(WRITE ${CMAKE_CURRENT_LIST_DIR}/../include/traffic-monitor/paths.h
+  FILE(WRITE ${CMAKE_CURRENT_LIST_DIR}/../include/traffic_monitor/paths.h
     "// Auto-generated headers file, do not edit, will be overwritten by cmake.
     // (c) 2017 Vigilatore \n
     #ifndef INCLUDE_TRAFFIC_MONITOR_CONFIG_H_
